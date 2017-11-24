902738a196c43ac7e42c20847e248164e7f28075
Ge11ert <gellert94@mail.ru>
2017-11-25 02:23:59 +0300

    Добавляет отрисовку пинов на карте

--- js/map.js
+++ js/map.js
@@ -49,15 +49,21 @@
     Y_MAX: 500
   };
 
-  var MAX_GUESTS = 10;
+  /** @const {number} */
+  var MAX_GUESTS = 7;
 
   var userIndex = 0;
   var lastUsedTitle = 0;
   var adverts = createAdvertsArray();
-  console.log(adverts);
+
+  var map = document.querySelector('.map__pins');
+  var mapPins = createPinsFromData(adverts);
+  var mapPinsFragment = renderAllPins(mapPins);
+
+  /** ------------------- Functions of MODEL component ------------------- */
 
   /**
-   *
+   * Fills up an array with generated adverts
    * @return {Array.<Advert>}
    */
   function createAdvertsArray() {
@@ -101,6 +107,9 @@
     return advert;
   }
 
+  /**
+   * @return {string} - Path to user image
+   */
   function getUserAvatar() {
     var pathBase = 'img/avatars/user0';
     if (userIndex < ADVERTS_AMOUNT) {
@@ -111,31 +120,51 @@
     }
   }
 
+  /**
+   * @return {string} - A title for a new advert
+   */
   function getOfferTitle() {
     var title = TITLES[lastUsedTitle];
     lastUsedTitle++;
     return title;
   }
 
+  /**
+   * @return {string} - Accommodation type of a new advert
+   */
   function getHouseType() {
     var typeIndex = getRandomFromRange(0, TYPES.length - 1).toFixed();
     return TYPES[typeIndex];
   }
 
+  /**
+   * @return {number}
+   */
   function getRoomsQuantity() {
     return Math.floor(getRandomFromRange(RoomsAmount.MIN, RoomsAmount.MAX));
   }
 
+  /**
+   * @return {string}
+   */
   function getCheckinTime() {
     var timeIndex = getRandomFromRange(0, CHECKIN_TIMES.length - 1).toFixed();
     return CHECKIN_TIMES[timeIndex];
   }
 
+  /**
+   * @return {string}
+   */
   function getCheckoutTime() {
     var timeIndex = getRandomFromRange(0, CHECKOUT_TIMES.length - 1).toFixed();
     return CHECKOUT_TIMES[timeIndex];
   }
 
+  /**
+   * Generates a random set of advert features. In each iteration of 'for loop',
+   * arrayOfIndexes indicates the features, which weren't already used.
+   * @return {Array} - Array of random chosen features
+   */
   function getFeaturesList() {
     var featuresArrayLength = getRandomFromRange(1, FEATURES_LIST.length).toFixed();
     var featuresArray = [];
@@ -162,5 +191,55 @@
   function getRandomFromRange(min, max) {
     return Math.random() * (max - min) + min;
   }
+
+  /** ------------------- Functions of VIEW component ------------------- */
+
+  /**
+   * Creates a DOM Element 'Map Pin' using data from a single advert
+   * @param {Advert} advert
+   * @return {Element}
+   */
+  function renderMapPin(advert) {
+    var pinWidth = 40;
+    var pinHeight = 40;
+    var offset = {
+      x: pinWidth / 2,
+      y: pinHeight
+    };
+
+    var pin = document.createElement('button');
+    var avatar = document.createElement('img');
+
+    pin.classList.add('map__pin');
+    pin.style.left = advert.location.x - offset.x + 'px';
+    pin.style.top = advert.location.y - offset.y + 'px';
+
+    avatar.src = advert.author.avatar;
+    avatar.width = 40;
+    avatar.height = 40;
+    avatar.draggable = false;
+
+    pin.appendChild(avatar);
+    return pin;
+  }
+
+  function createPinsFromData(advertsArray) {
+    var pinsArray = [];
+    advertsArray.forEach(function (item) {
+      pinsArray.push(renderMapPin(item));
+    });
+    return pinsArray;
+  }
+
+  function renderAllPins(pinsArray) {
+    var fragment = document.createDocumentFragment();
+
+    pinsArray.forEach(function (pin) {
+      fragment.appendChild(pin);
+    });
+    return fragment;
+  }
+
+  map.appendChild(mapPinsFragment);
 
 })();
